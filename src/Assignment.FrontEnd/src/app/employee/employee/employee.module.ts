d7a1f9062210c74921b8cdcb5f3dea99852827e9
Hasibul Islam Shanto <bdshanto@hotmail.com>
2021-01-28 15:55:22 +0600

    config file setup

--- src/Assignment.FrontEnd/src/app/employee/employee/employee.module.ts
+++ src/Assignment.FrontEnd/src/app/employee/employee/employee.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { MatOptionModule } from '@angular/material/core';
 import { commonImports } from '../../app-config/appModuleConfig';
 import { EmployeeListComponent } from './components/employee-list/employee-list.component';
 import { EmployeeComponent } from './components/employee/employee.component';
@@ -6,7 +7,7 @@ import { employeeExports, empProviders } from './config/employee_module_config';
 
 @NgModule({
   declarations: [EmployeeComponent, EmployeeListComponent],
-  imports: [commonImports],
+  imports: [commonImports, MatOptionModule],
   exports: [employeeExports],
   providers: [empProviders]
 })
