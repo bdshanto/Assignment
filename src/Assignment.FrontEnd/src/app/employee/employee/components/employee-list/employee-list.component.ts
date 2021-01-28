38c1122469b0c6d4041dbff68bf2684ffeded376
bdshanto <bdshanto@hotmail.com>
2021-01-29 00:02:21 +0600

    material data table added

--- src/Assignment.FrontEnd/src/app/employee/employee/components/employee-list/employee-list.component.ts
+++ src/Assignment.FrontEnd/src/app/employee/employee/components/employee-list/employee-list.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { EmployeeService } from '../../employee.service';
+import { Employee } from '../../models/employee';
 
 @Component({
   selector: 'app-employee-list',
   templateUrl: './employee-list.component.html',
   styleUrls: ['./employee-list.component.scss']
 })
-export class EmployeeListComponent implements OnInit {
+export class EmployeeListComponent implements OnInit, AfterViewInit {
+  employees: Employee[] = [];
 
-  constructor() { }
+  displayedColumns: string[] = ['id', 'name', 'email', 'address', 'createOn', 'action'];
+  dataSource = new MatTableDataSource<Employee>(this.employees);
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  constructor(
+    private _service: EmployeeService
+  ) { }
 
   ngOnInit(): void {
+    this._service.getAll().subscribe((data: Employee[]) => {
+      if (data.length > 0) { this.employees = data; }
+    });
   }
 
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  Edit(id: number): void {
+
+  }
 }
